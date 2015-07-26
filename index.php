d47eb1db6e4244e48fdf2adb5d1bfb3ec62dde04
baydiwo-pc <baydiwo@gmail.com>
2015-07-26 15:59:18 +0700

    Section map left.

---
+++ index.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Palapa Jaya</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body class="loading">
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        <main>
+
+            <section id="slide-1" class="homeSlide slide-1">
+                <div class="bcg" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" data-anchor-target="#slide-1">
+                    <div class="hsContainer">
+                        <div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
+                            <h2>palapa jaya</h2>
+                            <p>transportation service</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="slide-2">
+                <div class="bcg" data-anchor-target="#slide-2">
+                    <div class="hsContainer">
+                        <div class="hsContent">
+                            <h2 data-center="opacity: 1" data-400-bottom="opacity: 0.3" data-500-top="opacity: 0.3" data-anchor-target="#slide-2 h2"><img src="img/motto.png" height="286" width="819"></h2>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="slide-3" class="homeSlide slide-3">
+                <div class="bcg" data-center="background-position: 0px 50%;" data-bottom-top="background-position: 0px 40%;" data-top-bottom="background-position: -40px 50%;" data-anchor-target="#slide-3">
+                    <div class="hsContainer">
+                        <div class="hsContent">
+                            <div class="services">
+                                <div class="col-50">
+                                    <div id="boxes-1" class="boxes" data-250-top="opacity: 0;left:-700px" data-100-top="opacity: 1;left:0;" data-bottom="opacity: 1;left:0;"  data-anchor-target="#slide-3">
+                                        <!-- Image Caption 6 -->
+                                        <div id="box-6" class="box">
+                                        <img id="image-6" src="img/packet-loading.jpg"/>
+                                            <span class="caption scale-caption">
+                                                <p><img src="img/icon-package.png"></p>
+                                                <h4>CAREFULL AND SAFETY <br> IS FIRST</h4>
+                                            </span>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <h3>paket loading</h3>
+                                        <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                                    </div>
+                                    <div id="boxes-2" class="boxes boxes-2" data-400-top="opacity: 0;left:-700px" data-200-top="opacity: 1;left:0;" data-bottom="opacity: 1;left:0;"  data-anchor-target="#boxes-2">
+                                        <!-- Image Caption 6 -->
+                                        <div id="box-6" class="box">
+                                        <img id="image-6" src="img/packet-loading.jpg"/>
+                                            <span class="caption scale-caption">
+                                                <p><img src="img/icon-package.png"></p>
+                                                <h4>CAREFULL AND SAFETY <br> IS FIRST</h4>
+                                            </span>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <h3>paket loading</h3>
+                                        <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                                    </div>
+                                </div>
+                                <div class="col-50">
+                                    <div id="boxes-3" class="boxes" data-250-top="opacity: 0;right:-700px" data-50-top="opacity: 1;right:0;" data-bottom="opacity: 1;right:0;"  data-anchor-target="#slide-3">
+                                        <!-- Image Caption 6 -->
+                                        <div id="box-6" class="box">
+                                        <img id="image-6" src="img/packet-loading.jpg"/>
+                                            <span class="caption scale-caption">
+                                                <p><img src="img/icon-package.png"></p>
+                                                <h4>CAREFULL AND SAFETY <br> IS FIRST</h4>
+                                            </span>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <h3>paket loading</h3>
+                                        <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                                    </div>
+                                    <div id="boxes-4" class="boxes boxes-2" data-400-top="opacity: 0;right:-700px" data-150-top="opacity: 1;right:0;" data-bottom="opacity: 1;right:0;"  data-anchor-target="#boxes-2">
+                                        <!-- Image Caption 6 -->
+                                        <div id="box-6" class="box">
+                                        <img id="image-6" src="img/packet-loading.jpg"/>
+                                            <span class="caption scale-caption">
+                                                <p><img src="img/icon-package.png"></p>
+                                                <h4>CAREFULL AND SAFETY <br> IS FIRST</h4>
+                                            </span>
+                                        </div>
+                                        <div class="clear"></div>
+                                        <h3>paket loading</h3>
+                                        <p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section id="slide-5" class="homeSlide homeSlideTall2">
+                <div class="bcg">
+                    &nbsp;
+                </div>
+                <div class="bcg bcg2" data-bottom-top="opacity: 0;" data--33p-top="opacity: 0;" data--66p-top="opacity: 1;" data-anchor-target="#slide-5">
+                    <div class="hsContainer">
+                        <div class="hsContent" data-bottom-top="opacity: 0;" data-center="opacity: 1" data-anchor-target="#slide-5">
+                            <div class="our-clients">
+                                <h2>our client</h2>
+                                <ul>
+                                    <li><img src="img/client-ajinomoto.png" height="109" width="161"></li>
+                                    <li><img src="img/client-mustika-ratu.png" height="118" width="165" alt=""></li>
+                                    <li><img src="img/client-linfox.png" height="120" width="236" alt=""></li>
+                                    <li><img src="img/client-sariayu.png" height="118" width="116" alt=""></li>
+                                    <li><img src="img/client-gac.png" height="114" width="263" alt=""></li>
+                                    <li><img src="img/client-kino.png" height="60" width="225" alt=""></li>
+                                    <li><img src="img/client-loreal.png" height="60" width="333" alt=""></li>
+                                    <li><img src="img/client-sanbe.png" height="60" width="249" alt=""></li>
+                                    <li><img src="img/client-breeze.png" height="63" width="128" alt=""></li>
+                                    <li><img src="img/client-purba-sari.png" height="120" width="241" alt=""></li>
+                                    <li><img src="img/client-nutrifood.png" height="120" width="200" alt=""></li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bcg bcg3" data-300-bottom="opacity: 0;" data-100-bottom="opacity: 1;" data-anchor-target="#slide-5">
+                    <div class="hsContainer">
+                        <div class="hsContent" data-100-bottom="opacity: 0;" data-bottom="opacity: 1;" data-anchor-target="#slide-5">
+                            <div id="map"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </main>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="js/imagesloaded.js"></script>
+        <script src="js/skrollr.js"></script>
+        <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.9/gmaps.min.js"></script>
+        <script src="js/_main.js"></script>
+
+    </body>
+
+</html>
